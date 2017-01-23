96d445b8356d38ffe14746b56bd023c4eb859e74
pancake <pancake@nopcode.org>
2017-01-23 02:24:24 +0100

    Update CONFIG for 1.2.0-git

--- main.sh
+++ main.sh
@@ -4,6 +4,60 @@
 . build.sh
 . publish.sh
 
+release_all() {
+	download radare2
+	download_others
+
+	# docker_linux_build x86 static
+	# TODO: dockerify
+	android_build x86
+	android_build mips
+	android_build arm
+	android_build aarch64
+
+	docker_linux_build x86
+	docker_linux_build x64
+
+	docker_linux_build armv5
+	# rpi? must test
+	#docker_linux_build armv6
+	#docker_linux_build armv7
+	#docker_linux_build armv64
+	#docker_linux_build mipsel
+	docker_linux_build mipsel static
+
+	docker_windows_build x86_64-w64-mingw32.static-gcc
+	docker_windows_build i686-w64-mingw32.static-gcc
+
+	case "`uname`" in
+	Darwin)
+		osx_build
+		ios_build arm
+		ios_build arm64
+		ios_appstore arm
+		ios_appstore arm64
+		;;
+	Linux)
+		# linux_build
+		:
+		;;
+	esac
+
+	publish_checksums
+
+	### populate all binaries
+	publish_cydia
+	publish_out
+	publish_android
+
+	### announce
+	publish_irc
+	publish_www
+	# publish_twitter
+	# publish_telegram
+	# publish_blog
+}
+
 case "$1" in
 -l)
 	cat build.sh | grep '()' | grep build | awk -F '_build' '{print $1}'
@@ -68,57 +122,3 @@ ios:
 	;;
 esac
 
-release_all() {
-
-	download radare2
-	download_others
-
-	# docker_linux_build x86 static
-	# TODO: dockerify
-	android_build x86
-	android_build mips
-	android_build arm
-	android_build aarch64
-
-	docker_linux_build x86
-	docker_linux_build x64
-
-	docker_linux_build armv5
-	# rpi? must test
-	#docker_linux_build armv6
-	#docker_linux_build armv7
-	#docker_linux_build armv64
-	#docker_linux_build mipsel
-	docker_linux_build mipsel static
-
-	docker_windows_build x86_64-w64-mingw32.static-gcc
-	docker_windows_build i686-w64-mingw32.static-gcc
-
-	case "`uname`" in
-	Darwin)
-		osx_build
-		ios_build arm
-		ios_build arm64
-		ios_appstore arm
-		ios_appstore arm64
-		;;
-	Linux)
-		# linux_build
-		:
-		;;
-	esac
-
-	publish_checksums
-
-	### populate all binaries
-	publish_cydia
-	publish_out
-	publish_android
-
-	### announce
-	publish_irc
-	publish_www
-	# publish_twitter
-	# publish_telegram
-	# publish_blog
-}
