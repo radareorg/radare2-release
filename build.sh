9829436b8a2385923c7c7dbafa14e747189aeb13
pancake <pancake@nopcode.org>
2018-09-12 20:40:57 +0200

    Remove hardcoded r2frida version

--- build.sh
+++ build.sh
@@ -171,15 +171,15 @@ docker_linux_r2frida_build() {(
 			git clone --depth 20 https://github.com/nowsecure/r2frida tmp/r2frida
 		fi
 		(
-			cp -f out/2.9.0/radare2_2.9.0_amd64.deb tmp/r2frida
-			cp -f out/2.9.0/radare2-dev_2.9.0_amd64.deb tmp/r2frida
+			cp -f out/${VERSION}/radare2_${VERSION}_amd64.deb tmp/r2frida
+			cp -f out/${VERSION}/radare2-dev_${VERSION}_amd64.deb tmp/r2frida
 			cd tmp/r2frida
 			${CWD}/dockcross --image dockcross/linux-${arch} bash -c \
 			"export CFLAGS=-O2 ;
 			export ARCH=${debarch} ;
 			export MAKE='make V=1' ; 
-			sudo dpkg -i radare2_2.9.0_amd64.deb ;
-			sudo dpkg -i radare2-dev_2.9.0_amd64.deb ;
+			sudo dpkg -i radare2_${VERSION}_amd64.deb ;
+			sudo dpkg -i radare2-dev_${VERSION}_amd64.deb ;
 			make -j2"
 			cd dist/debian && make purge && make >> ${LOG}
 		)
